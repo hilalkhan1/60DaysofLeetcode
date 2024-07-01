85007ef954585192fe4193d8696b65f4922c830c
Hilal Khan <106275724+hilalkhan1@users.noreply.github.com>
2024-07-01 23:18:06 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 1/pattern-1.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def printSquare(self, N):
+        # Code here
+        for i in range(N):
+            print("*", end=' ')
+            for j in range(1, N):
+                print('*', end=" ")
+            print()
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printSquare(N)
+# } Driver Code Ends
